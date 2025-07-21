494cb2d794e4e4635ff1c64c7331e29cb24ac6df
Aritra Dey <adey01027@gmail.com>
2025-07-21 23:32:38 +0530

    fix:auth issue
    Signed-off-by: Aritra Dey <adey01027@gmail.com>

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
-type props = {
-  children: React.ReactNode;
-};
+type Props = { children: React.ReactNode }
 
-const Layout = ({ children }: props) => {
-  return <div className="flex justify-center
-  items-center
-  h-screen
-  w-full">{children}</div>;
-};
-export default Layout;
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+      {children}
+    </div>
+  )
+}
+
+export default Layout
